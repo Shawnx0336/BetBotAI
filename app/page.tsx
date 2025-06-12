868a832dcb17419b9234a88a50ec78f5bdc45d1b
Shawn Davis <shawn.davis@asd-web.com>
2025-06-12 08:04:06 -0400

    Fix duplicate import statements in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
-import { handleApiError } from '../utils/apiHelpers';
 import { handleApiError, getCachedData, setCachedData } from '../utils/apiHelpers';
 
 // REQUIRED INTERFACES
